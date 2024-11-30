0f2d666d7e7beb10a0371de97bf82560ed230435
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-30 07:59:02 +0700

    Add listener onButton

--- app/src/main/java/com/example/alab2/MainActivity.kt
+++ app/src/main/java/com/example/alab2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.alab2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputMinIncremental = findViewById<EditText>(R.id.inputMinIncremental)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+
+        calculateButton.setOnClickListener { _ ->
+            val minIncremental = inputMinIncremental.text.toString().toDouble()
+            true
         }
     }
 }
