1634fe1f8cb21fa9632ca189d67908e51339a2cb
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-30 08:11:00 +0700

    Add output result

--- app/src/main/java/com/example/alab2/MainActivity.kt
+++ app/src/main/java/com/example/alab2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.alab2
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,10 +16,28 @@ class MainActivity : AppCompatActivity() {
 
         val inputMinIncremental = findViewById<EditText>(R.id.inputMinIncremental)
         val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val outputResult = findViewById<TextView>(R.id.outputResult)
 
         calculateButton.setOnClickListener { _ ->
             val minIncremental = inputMinIncremental.text.toString().toDouble()
+            outputResult.text = calculate(minIncremental).toString()
             true
         }
+    }
+
+    fun calculate(constrain: Double) : Double
+    {
+        var sum = 0.0
+        var denominator = 1.0
+        while (true) {
+            val nextTerm = 1.0 / (denominator * denominator)
+            sum += nextTerm
+            if (nextTerm > constrain) {
+                denominator++
+            } else {
+                break
+            }
+        }
+        return sum
     }
 }
